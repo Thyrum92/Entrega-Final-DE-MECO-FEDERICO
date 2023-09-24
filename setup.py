8e2755ab1bde961188524d63caa2e01a8ba56724
Federico De Meco <federico.demeco@hotmail.com>
2023-09-23 21:08:32 -0300

    Eliminacion de archivos innecesarios

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(
-    name="paquetes",
-    version= "1.0",
-    packages=find_packages(),
-    description="Este paquete contiene las funciones y clases del proyecto",
-    author="fdm",
-    author_email="fdm@asduhaisydgasd.com")
